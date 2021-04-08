fc83b771bf74c79afe84c535216726b40f43a652
DESKTOP-470UPS0\atrop <atropose76@gmail.com>
2021-04-09 07:20:46 +0900

    Maven Test

---
+++ src/main/java/hello/GreeterTest.java
@@ -0,0 +1,17 @@
+package hello;
+
+import static org.junit.matchers.JUnitMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+  
+  private Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello() {
+    assertThat(greeter.sayHello(), containsString("Hello"));
+  }
+
+}
